77bbf4c05c50539d095de025e7fe382a4dfc8ee6
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-14 22:02:55 -0300

    Adicionando Router e criando componente <Home />

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
 
 function App() {
   return (
-    <div>Teste</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import GlobalStyle from './styles/global'
 import GlobalThemeProvider from './styles/theme'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalThemeProvider>
-      <GlobalStyle />
-      <App />
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
     </GlobalThemeProvider>
   </React.StrictMode>
 )
