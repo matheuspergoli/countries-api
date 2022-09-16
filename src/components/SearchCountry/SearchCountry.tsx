66fcdd7284a28fae3dd1a104834bb627644d2311
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-09-15 23:14:28 -0300

    Estilizando campo de pesquisa

--- src/components/SearchCountry/SearchCountry.tsx
+++ src/components/SearchCountry/SearchCountry.tsx
@@ -5,11 +5,36 @@ import useFetch from '../../Hooks/useFetch'
 import Country from '../Country/Country'
 
 const FormContainer = styled.form`
+  max-width: 400px;
+  margin-left: 50px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 
+  @media (max-width: 480px) {
+    margin-left: 20px;
+    margin-right: 20px;
+  }
 `
 
 const FormGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
 
+  input {
+    width: 100%;
+    border: none;
+    font-size: 1.1rem;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  svg {
+    font-size: 1.5rem;
+  }
 `
 
 function SearchCountry() {
